701927c429dbce2a63449fd14102808c98d00721
José Roberto de Badran Borges <betobadran@live.com>
2020-05-23 16:59:10 -0300

    modified:   src/main/java/br/com/badr/zipcode/br/controller/CepRestController.java 	modified:   src/main/resources/application.properties

--- src/main/java/br/com/badr/zipcode/br/controller/CepRestController.java
+++ src/main/java/br/com/badr/zipcode/br/controller/CepRestController.java
@@ -1,5 +1,9 @@
 package br.com.badr.zipcode.br.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +20,15 @@ public class CepRestController {
 	private CepService mCepService;
 	
 	@GetMapping(value = "/api/br/json/{CEP}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public CepIf getZipCode(@PathVariable(value = "CEP") String pCep) {
+	public CepIf getZipCode(@PathVariable(value = "CEP") String pCep, 
+			HttpServletResponse pRsp) throws IOException {
+		if (pCep == null ||
+			pCep.isEmpty() ||
+			pCep.trim().length() > 9 || 
+			pCep.trim().length() < 8) {
+			pRsp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Cep format allowed: xxxxxxxx or xxxxx-xxx");
+			return null;
+		}
 		return mCepService.getCep(pCep);
 	}
 
