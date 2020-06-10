282181c62c879330fde500819ab15094c0896420
José Roberto de Badran Borges <betobadran@live.com>
2020-06-09 23:39:45 -0300

    test prometheus

--- src/main/java/br/com/badr/AppStart.java
+++ src/main/java/br/com/badr/AppStart.java
@@ -5,11 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnablePrometheusMetrics
 public class AppStart {
 
 	public static void main(String[] args) throws Exception {
